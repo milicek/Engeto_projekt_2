473749ee12c21e0300a5abd5095b7aaca82aae45
Michal Uryč <michal.uryc@seznam.cz>
2025-03-27 18:21:12 +0100

    pokračování

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+projekt_1.py: první projekt do Engeto Online Python Akademie
+
+author: Michal Uryč
+email: michal.uryc@seznam.cz
+"""
+#Bull & Cows - hra postavená na hádání 4 ciferného čísla
+
+#import knihoven
+from random import randint
+
+
